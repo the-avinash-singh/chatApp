d6f709f5ca4e72e4946dc8fa0ea63af266561463
Avinash singh <106461441+the-avinash-singh@users.noreply.github.com>
2023-11-24 08:49:23 +0530

    changing the scroll direction

--- src/components/ChatSection.tsx
+++ src/components/ChatSection.tsx
@@ -1,51 +1,52 @@
 //@ts-nocheck
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "../styles/chat-section.css";
 import Sender from "./SenderMessage";
 import Reciver from "./ReciverMessage";
 import { fetchMoreData } from "../Apis/chatapi";
-import InfiniteScroll from "react-infinite-scroll-component";
+import InfiniteScroll from 'react-infinite-scroller';
 import Loader from "../utils/Loader";
 
-const ChatSection = ({ chatData }) => {
+const ChatSection = ({ chatingData }) => {
 
   const [page, setPage] = React.useState(0);
 
   const [chatsData, setChatData] = React.useState([]);
-
+  
+  const scroll=useRef(null)
+  const div1=useRef(null)
   const infiniteApi = async () => {
-    const data = await fetchMoreData(page + 1);
-    setChatData(chatsData.concat(data.chats));
+    const data = await fetchMoreData(page+1);
     setPage(page + 1);
+    setChatData(chatsData.concat(data.chats));
+      
   };
-
+  
   useEffect(() => {
-    setChatData(chatData);
-  }, [chatData]);
-
-  if (chatsData === undefined) {
-    return;
-  }
-
+    div1.current.scrollIntoView({ block: "end" })
+  });
+  
   return (
-    <div className="chat-div">
-      <InfiniteScroll
-        dataLength={chatsData.length}
-        next={infiniteApi}
-        hasMore={true}
-        loader={<Loader />}
-      >
-        <div className="d-flex flex-column gap mx-auto pt-4 px-3 custom-width">
+    <div className="chat-div" >
+        <div className="d-flex flex-column-reverse gap mx-auto pt-4 px-3 pb-3 custom-width" ref={div1}>
+        <InfiniteScroll
+    loadMore={infiniteApi}
+    style={{ display: 'flex', flexDirection: 'column-reverse',gap:"1.2rem" }} 
+    isReverse={true}
+    hasMore={true}
+  >
           {chatsData?.map((data, index) => {
             return (
-              <div key={index} className="d-flex flex-column gap">
+              <div key={index}>
                 {data.sender.self?<Reciver data={data} />
                 :<Sender data={data}/>}
               </div>
             );
           })}
+          </InfiniteScroll>
+          <Loader/>
         </div>
-      </InfiniteScroll>
+
     </div>
   );
 };
