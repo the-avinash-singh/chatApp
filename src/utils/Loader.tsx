6b3bc770dad07c62141c55b847bfd73357f38f63
Avinash singh <106461441+the-avinash-singh@users.noreply.github.com>
2023-11-24 08:51:26 +0530

    put in key

--- src/utils/Loader.tsx
+++ src/utils/Loader.tsx
@@ -3,7 +3,7 @@ import loader from "../assets/loader.gif"
 export default function Loader() {
   
     return (
-      <div className='text-center'>
+      <div className='text-center' key={0}>
         <img className='my-3' src={loader} alt="loading..."/>
       </div>
     )
